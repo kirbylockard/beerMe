dfc620d5f030d4456559780e509977bf9cd1e6e0
Kirby Lockard <kirbylockard@gmail.com>
2022-05-02 19:03:23 -0600

    direct to err message if there are empty search params or zero results. Very basic form validation, needs to be improved

--- findBeer.js
+++ findBeer.js
@@ -3,15 +3,6 @@
 const base = "https://api.openbrewerydb.org/breweries?";
 
 const findBeer = async function (base, searchMethod, searchTerm) {
-  //check search method
-  if (searchMethod === "state") {
-    searchMethod = "by_state=";
-  } else if (searchMethod === "city") {
-    searchMethod = "by_city=";
-  } else if (searchMethod === "zip") {
-    searchMethod = "by_postal=";
-  }
-  console.log(searchMethod);
   const response = await fetch(`${base + searchMethod + searchTerm}`); //returns a promise
   const data = await response.json();
   return data;
