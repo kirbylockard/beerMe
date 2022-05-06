c2dea8d7891c8d95e60733bc5654f287c79ec7d4
Kirby Lockard <kirbylockard@gmail.com>
2022-05-06 14:03:12 -0600

    successfully made api call to retrieve US States. Next, replace state and city input fields with select menus

--- findBeer.js
+++ findBeer.js
@@ -16,5 +16,26 @@ const findBeer = async function (searchMethods, encodedTerms, page) {
   const response = await fetch(`${base + query}`); //returns a promise
   console.log(response);
   const data = await response.json();
+  console.log(data);
   return data;
 };
+
+//UniversalTutorial API Token
+//xgb0ZAnpXGIckE66A39FjBw9akzF0vozFTGNrxFfrLRDKF3zcfl5p__xGVaMeCnpAZQ
+
+const getToken = async function () {
+  const response = await fetch("https://www.universal-tutorial.com/api/getaccesstoken", {
+    headers: new Headers({ Accept: "application/json", "api-token": "xgb0ZAnpXGIckE66A39FjBw9akzF0vozFTGNrxFfrLRDKF3zcfl5p__xGVaMeCnpAZQ", "user-email": "kirbylockard@gmail.com" })
+  });
+  return response.json();
+};
+
+const getLocations = async function (token) {
+  const response = await fetch("https://www.universal-tutorial.com/api/states/United States", {
+    headers: new Headers({ Authorization: `Bearer ${token}`, Accept: "application/json" })
+  });
+  const data = await response.json();
+  console.log(data);
+};
+
+getToken().then(data => getLocations(data.auth_token));
